31cf4044e6e02787102b4db4eab7b09912b2f0d4
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-05-20 14:34:49 -0700

    clean af

--- src/pages/dev/index.js
+++ src/pages/dev/index.js
@@ -1,94 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./index.module.scss";
-import Featured from "./featured";
-import FullStack from "./featured";
-import WIP from "./featured";
 import {Helmet} from "react-helmet";
+import store from "../../database/index2021";
 
-class Dev extends React.Component {
-  state = {
-    workShown: false,
-    uxShown: false,
-    resourcesShown: false,
-    featuredShown: true,
-    pageTitle: "Featured Projects",
+export default function Dev() {
+
+  const [filteredProjects, setfilteredProjects] = useState([]);
+  const [databaseProjects] = useState(store.projects);
+  const [pageTitle, setPageTitle] = useState("Featured");
+
+  
+
+  function onResetArray() {
+    //setfilteredProjects([]);
+    const onlyInclude = databaseProjects.filter((passedItem) => passedItem.tags.includes("featured"));
+    setfilteredProjects(onlyInclude);
+    setPageTitle("Featured")
   };
 
-  renderFeatured() {
-    return <Featured />;
-  }
-
-  renderFullStack() {
-    return <FullStack />;
-  }
-
-  renderWIP() {
-    return <WIP />;
-  }
-
-  showProgramming = (e) => {
-    const name = e.target.name;
-    if (name === "featured") {
-      this.setState({
-        featuredShown: false,
-        workShown: true,
-        uxShown: false,
-        resourcesShown: false,
-        pageTitle: "Featured Projects",
-      });
-      this.renderFeatured();
-    }
-    if (name === "fullstack") {
-      this.setState({
-        featuredShown: false,
-        workShown: false,
-        uxShown: true,
-        resourcesShown: false,
-        pageTitle: "Full Stack",
-      });
-      this.renderFullStack();
-    }
-    if (name === "wip") {
-      this.setState({
-        featuredShown: false,
-        workShown: false,
-        uxShown: false,
-        resourcesShown: true,
-        pageTitle: "WIP",
-      });
-      this.renderWIP();
-    }
+  function filterFullstack() {
+    const onlyInclude = databaseProjects.filter((passedItem) => passedItem.tags.includes("fullstack"));
+    setfilteredProjects(onlyInclude);
+    setPageTitle("Fullstack")
   };
 
-  render() {
-    console.log("Page Title: " + this.state.pageTitle);
-    return (
+  function filterWIP() {
+    const onlyInclude = databaseProjects.filter((passedItem) => passedItem.tags.includes("wip"));
+    setfilteredProjects(onlyInclude);
+    setPageTitle("WIP")
+  };
+
+  return (
       <div>
         <Helmet>
           <title>Development</title>
           <meta name="description" content="Lorelei Miyamura's Development Work"/>
         </Helmet>
         <section className={style.workContainer}>
-          <h1>{this.state.pageTitle}</h1>
+          <h1>{pageTitle}</h1>
           <div className={style.filterContainer}>
-            <button name="featured" onClick={this.showProgramming} className={this.state.pageTitle === "Featured Projects" ? style.selected: ""}>
-              Featured
-            </button>
-            <button name="fullstack" onClick={this.showProgramming} className={this.state.pageTitle === "Full Stack" ? style.selected: ""}>
-              Full Stack
-            </button>
-            <button name="wip" onClick={this.showProgramming} className={this.state.pageTitle === "WIP" ? style.selected: ""}>
-              WIP
-            </button>
+            <button onClick={()=> onResetArray()} className={pageTitle=== "Featured" ? style.selected: ""}>Featured</button>
+            <button onClick={()=> filterFullstack()} className={pageTitle=== "Fullstack" ? style.selected: ""}>Fullstack</button>
+            <button onClick={()=> filterWIP()}  className={pageTitle=== "WIP" ? style.selected: ""}>WIP</button>
           </div>
-          {this.state.featuredShown && this.renderFeatured()}
-          {this.state.workShown && this.renderFeatured()}
-          {this.state.uxShown && this.renderFullStack()}
-          {this.state.resourcesShown && this.renderWIP()}
+          <AutoList databaseProjects={filteredProjects.length > 0 ? filteredProjects : databaseProjects} />
         </section>
       </div>
-    );
-  }
+
+  );
 }
 
-export default Dev;
+const AutoList = ({ databaseProjects }) => {
+  return databaseProjects.map((a, i) => 
+  <section key={a.name} className={style.projectCard}
+  onClick={(event) => {
+    event.preventDefault();
+    window.open(a.github);
+  }}>
+      <div className={style.imageCrop}>
+        <img src={a.image} alt='thumbnail of project'/>
+      </div>
+      <div className={style.overlay}></div>
+      <div className={style.projectDetails}>
+        <h4>{a.name}</h4>
+        <h5>{a.collab}</h5>
+      </div>
+  </section>);
+};
