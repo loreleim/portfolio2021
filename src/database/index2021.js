6895e178df1cb72c67e68a1cbd785927eba2fea8
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-05-21 00:05:13 -0700

    organizing data + adding image thumbnails

--- src/database/index2021.js
+++ src/database/index2021.js
@@ -2,9 +2,9 @@ class Database {
   constructor() {
     this.projects = [
       {
-        name: "Training Modules",
-        collab: "Pollinate x Oregon Lottery",
-        image: "../images/bus-01.jpg",
+        name: "ALA Tool",
+        collab: "American Libraries Association",
+        image: "../images/ala-0.jpg",
         link: "/bus",
         github: "https://github.com/loreleim/da-bus",
         tags: "featured, fullstack"
@@ -12,18 +12,18 @@ class Database {
       {
         name: "Passion to Purpose",
         collab: "Convergence Design Lab",
-        image: "../images/bus-01.jpg",
+        image: "../images/p2p.gif",
         link: "/convergence",
         github: "https://convergencedesignlab.org/",
         tags: "featured, fullstack"
       },
       {
-        name: "Wordpress Theme",
-        collab: "Convergence Design Lab",
+        name: "Training Modules",
+        collab: "Pollinate x Oregon Lottery",
         image: "../images/bus-01.jpg",
         link: "/bus",
         github: "https://github.com/loreleim/da-bus",
-        tags: "featured, wordpress"
+        tags: "featured, fullstack"
       },
       {
         name: "Annex Art Drop",
@@ -34,12 +34,20 @@ class Database {
         tags: "featured"
       },
       {
-        name: "Research Anthology",
-        collab: "Hawai'i Industry Survey 2020",
+        name: "Industry Survey",
+        collab: "Paginated Survey to Google Sheets",
         image: "../images/hawaii.gif",
         link: "/hisurvey",
         github: "https://loreleim.github.io/hawaiisurvey/",
         tags: "featured, fullstack"
+      },
+      {
+        name: "Wordpress Theme",
+        collab: "Convergence Design Lab",
+        image: "../images/cdlmockup-thumb.jpg",
+        link: "/bus",
+        github: "https://github.com/loreleim/da-bus",
+        tags: "featured, wordpress"
       },
       {
         name: "lauAPI",
@@ -52,7 +60,7 @@ class Database {
       {
         name: "daBus",
         collab: "the bus hawai’i api + sso",
-        image: "../images/hawaii.gif",
+        image: "../images/bus-01.jpg",
         link: "/hisurvey",
         github: "https://loreleim.github.io/hawaiisurvey/",
         tags: "wip"
